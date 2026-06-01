b338f881667c2de63a5881cb7fdeaaaf2d0911da
gdias2910 <guinascimento2007@gmail.com>
2026-06-01 18:13:23 -0300

    controls the API's private routes

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Token not informed" });
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+}
+
+module.exports = authMiddleware;
